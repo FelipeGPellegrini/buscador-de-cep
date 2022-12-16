2ba0dd4afb2179cafd8338a34f280248d205e382
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2022-12-16 19:40:25 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import "./styles.css";
+import api from "./services/api";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    // 26178370/json
+
+    if (input === "") {
+      alert("Preencha algum CEP!");
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert("Ops! erro ao buscar.");
+      setInput("");
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Digite seu CEP..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <AiOutlineSearch size={25} color="#fff" />
+        </button>
+      </div>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>{cep.logradouro}</span>
+          {cep.complemento !== "" && (
+            <span>Complemento: {cep.complemento}</span>
+          )}
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
+    </div>
+  );
+}
+
+export default App;
